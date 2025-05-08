1c582174e0994a25d11a1cfa2a48e4a5a393e076
nabihakhan6t4 <khannabiha923@gmail.com>
2025-05-08 17:49:40 +0300

    changes

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,6 +1,6 @@
 export const generateToken = (user, message, statusCode, res) => {
-  const token = user.generateJsonWebToken();
-  
+  const token = user.generateJsonWebToken(); // Make sure this method sets the JWT expiration
+
   // Convert COOKIE_EXPIRES from days to milliseconds
   const expiresInDays = parseInt(process.env.COOKIE_EXPIRES) || 7; // Default to 7 days if the value is not set
   const expiresInMilliseconds = expiresInDays * 24 * 60 * 60 * 1000; // 7 days in milliseconds
@@ -8,7 +8,7 @@ export const generateToken = (user, message, statusCode, res) => {
   res
     .status(statusCode)
     .cookie("token", token, {
-      expires: new Date(Date.now() + expiresInMilliseconds), // Set the expiration
+      expires: new Date(Date.now() + expiresInMilliseconds), // Set the expiration for the cookie
       httpOnly: true,
     })
     .json({
@@ -18,4 +18,3 @@ export const generateToken = (user, message, statusCode, res) => {
       token,
     });
 };
-
