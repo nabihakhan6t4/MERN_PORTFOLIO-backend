a25ca96f7d183459016954aa18850f9ac7d1a7c5
nabihakhan6t4 <khannabiha923@gmail.com>
2025-05-12 16:00:22 +0300

    completed

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import app from "./app.js";
-import { v2 as cloudinary } from "cloudinary";
+import cloudinary from "cloudinary";
 
-cloudinary.config({
+cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
