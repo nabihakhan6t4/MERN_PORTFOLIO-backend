e3717cc613f634d995c4d62647ff67d942009775
nabihakhan6t4 <khannabiha923@gmail.com>
2025-05-12 15:14:41 +0300

    completed

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,7 +4,7 @@ import ErrorHandler from "./error.js";
 import jwt from "jsonwebtoken";
 
 export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
-   const token = req.headers['authorization']?.split(' ')[1];
+   const { token } = req.cookies;
   if (!token) {
     return next(new ErrorHandler("User not Authenticated!", 400));
   }
